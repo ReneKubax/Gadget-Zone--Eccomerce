d37491c418735c5e3746bb82419d663875badb48
Rene Fuentes <145807039+efuentes02@users.noreply.github.com>
2024-03-05 20:14:25 -0600

    feat: create alert when Api fail

--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -4,23 +4,39 @@ export const ProductContext = createContext();
 
 /**
  * Creates a product provider component that fetches and provides product data through context.
+ * Also manages loading state and error handling for the API call.
  *
  * @param {Object} children - The child components to be rendered within the provider.
  * @return {JSX.Element} The product provider component.
  */
 const ProductProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
     const fetchProducts = async () => {
-      const response = await fetch("https://fakestoreapi.com/products");
-      const data = await response.json();
-      setProducts(data);
+      setIsLoading(true);
+      try {
+        const response = await fetch("https://fakestoreapi.com/products/sasda/dsa");
+        if (!response.ok) {
+          throw new Error('Error fetching products');
+        }
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        setError(error.message);
+        alert(`Error: ${error.message}`); 
+      } finally {
+        setIsLoading(false); 
+      }
     };
+
     fetchProducts();
   }, []);
 
   return (
-    <ProductContext.Provider value={{ products }}>
+    <ProductContext.Provider value={{ products, isLoading, error }}>
       {children}
     </ProductContext.Provider>
   );
